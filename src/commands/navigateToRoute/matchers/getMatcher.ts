fdf3ff0036a495a3d91672105164403cd7555ef4
Nir Tamir <nirtamir2@gmail.com>
2021-09-21 20:59:43 +0300

    feat: add option to use nextjs search style in vue files

--- src/commands/navigateToRoute/matchers/getMatcher.ts
+++ src/commands/navigateToRoute/matchers/getMatcher.ts
@@ -2,12 +2,21 @@ import * as globby from "globby";
 import { MatcherType } from "../MatcherType";
 import { getNextMatcher } from "./nextMatcher";
 import { getNuxtMatcher, isVueFile } from "./nuxtMatcher";
+import * as vscode from "vscode";
+
+function shouldUseNuxtStyleInVueFiles() {
+  return (
+    vscode.workspace
+      .getConfiguration("router-explorer")
+      .get<string>("search.routes.vue.style") === "nuxt"
+  );
+}
 
 export function getMatcher(file: globby.Entry): {
   regexp: RegExp;
   type: MatcherType;
 } {
-  if (isVueFile(file.name)) {
+  if (isVueFile(file.name) && shouldUseNuxtStyleInVueFiles()) {
     return getNuxtMatcher(file);
   }
   return getNextMatcher(file);
